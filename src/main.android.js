fd49040d5dbce38deb9344bff50fcb4b48429a40
Siyuan Gao <siyuangao@gmail.com>
2016-04-18 21:02:01 -0700

    Adding redux

--- src/main.android.js
+++ src/main.android.js
@@ -1,15 +1,9 @@
-/**
- * React Native Webpack Starter Kit
- * https://github.com/jhabdas/react-native-webpack-starter-kit
- */
-import { AppRegistry } from 'react-native'
-import App from './components/App'
 
-class Root extends App {
-  static defaultProps = {
-    ...App.defaultProps,
-    instructions: 'Shake or press menu button for dev menu',
-  };
-}
+import React from "react-native";
+import Root from "./src/containers/root";
 
-AppRegistry.registerComponent('App', () => Root)
+const {
+  AppRegistry
+} = React;
+
+AppRegistry.registerComponent("App", () => Root);
