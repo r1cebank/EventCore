093c23f6129144a789dfb703014a9cd6f51c6968
Siyuan Gao <siyuangao@gmail.com>
2016-04-30 11:36:03 -0700

    Using react flux router

--- src/containers/root.js
+++ src/containers/root.js
@@ -1,18 +1,47 @@
 /* @flow */
 
-import React from 'react-native';
-import { Provider } from 'react-redux';
-import configureStore from '../store/configure-store';
-import Scene from '../components/scene';
+import React, { Text, View, StyleSheet } from 'react-native';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 
-const store = configureStore();
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
+});
+
+class Home extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+
+const scenes = Actions.create(
+    <Scene key="root">
+    <Scene key="home" component={Home} title="Home" />
+    </Scene>
+);
 
 class Root extends React.Component {
     render() {
         return (
-            <Provider store={store}>
-            <Scene />
-            </Provider>
+            <Router scenes={scenes} />
         );
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,68 @@
-/**
- * React Native Webpack Starter Kit
- * https://github.com/jhabdas/react-native-webpack-starter-kit
- */
-(function() {
-  'use strict'
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require('webpack-notifier');
 
-  var path = require('path');
-  var webpack = require('webpack');
-  var WebpackNotifierPlugin = require('webpack-notifier');
-
-  module.exports = {
+const config = {
     debug: true,
     devtool: 'source-map',
     plugins: [
         new WebpackNotifierPlugin()
     ],
     entry: {
-      'index.ios': ['./index.ios.js'],
-      'index.android': ['./index.android.js'],
+        'index.ios': ['./index.ios.js'],
+        'index.android': ['./index.android.js']
     },
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
     },
     module: {
-      preLoaders: [
-        {
-          test: /\.(js|jsx|es6)$/,
-          include: path.resolve(__dirname, 'src'),
-          loader: 'eslint-loader',
-        }
-      ],
-      loaders: [
-        {
-          test: /\.js$/,
-          include: /node_modules\/react-native/,
-          loader: 'babel',
-          query: {
-            cacheDirectory: true,
-            presets: ['es2015', 'stage-1', 'react']
-          }
-        },
-        {
-          test: /\.(js|jsx|es6)$/,
-          exclude: /node_modules/,
-          loader: 'babel',
-          query: {
-            cacheDirectory: true,
-            presets: ['es2015', 'stage-1', 'react']
-          }
-        }
-      ]
+        preLoaders: [
+            {
+                test: /\.(js|jsx|es6)$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'eslint-loader'
+            }
+        ],
+        loaders: [{
+            test: /\.js$/,
+            include: [
+                path.resolve(__dirname, 'src'),
+                path.resolve(__dirname, 'node_modules/react-native-navbar')
+            ],
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'stage-0', 'react']
+            }
+        }]
     }
-  }
-}())
+};
+
+// Hot loader
+if (process.env.HOT) {
+    config.devtool = 'eval'; // Speed up incremental builds
+    config.entry['index.ios'].unshift('react-native-webpack-server/hot/entry');
+    config.entry['index.ios'].unshift('webpack/hot/only-dev-server');
+    config.entry['index.ios'].unshift('webpack-dev-server/client?http://localhost:8082');
+    config.output.publicPath = 'http://localhost:8082/';
+    config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+    config.module.loaders[0].query.plugins.push('react-transform');
+    config.module.loaders[0].query.extra = {
+        'react-transform': {
+            transforms: [{
+                transform: 'react-transform-hmr',
+                imports: ['react-native'],
+                locals: ['module']
+            }]
+        }
+    };
+}
+
+// Production config
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = config;
