81be8ef13f0d8b31cb2f846f2464115269b42425
Siyuan Gao <siyuangao@gmail.com>
2016-06-15 22:12:37 -0700

    Refactor Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import { Platform, TouchableOpacity, View, Image, ToolbarAndroid } from 'react-native';
-import { Views, Components, Colors, Icons, Defaults, Utils, Assets } from '../../global/globalIncludes';
+import { Components, Colors } from '../../global/globalIncludes';
 
 const { Text } = Components.Text;
 
@@ -10,21 +11,33 @@ const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 20 : 25;
 const HEADER_HEIGHT = Platform.OS === 'ios' ? 44 + STATUS_BAR_HEIGHT : 56 + STATUS_BAR_HEIGHT;
 
 class F8HeaderAndroid extends React.Component {
+    static propTypes = {
+        leftItem: React.PropTypes.object,
+        rightItem: React.PropTypes.object,
+        extraItems: React.PropTypes.object,
+        title: React.PropTypes.string,
+        icon: React.PropTypes.string,
+        layout: React.PropTypes.string,
+        foreground: React.PropTypes.string,
+        children: React.PropTypes.element,
+        style: React.PropTypes.number
+    };
+
     render() {
-        const {leftItem, rightItem, extraItems} = this.props;
+        const { leftItem, rightItem, extraItems } = this.props;
         let actions = [];
         if (rightItem) {
-            const {title, icon, layout} = rightItem;
+            const { title, icon, layout } = rightItem;
             actions.push({
                 icon: layout !== 'title' ? icon : undefined,
-                title: title,
-                show: 'always',
+                title,
+                show: 'always'
             });
         }
         if (extraItems) {
             actions = actions.concat(extraItems.map((item) => ({
                 title: item.title,
-                show: 'never',
+                show: 'never'
             })));
         }
 
@@ -35,7 +48,7 @@ class F8HeaderAndroid extends React.Component {
         let content;
         if (React.Children.count(this.props.children) > 0) {
             content = (
-                <View collapsable={false} style={{flex: 1}}>
+                <View collapsable={false} style={{ flex: 1 }}>
                 {this.props.children}
                 </View>
             );
@@ -43,17 +56,17 @@ class F8HeaderAndroid extends React.Component {
 
         return (
             <View style={[Styles.toolbarContainer, this.props.style]}>
-            <ToolbarAndroid
-            navIcon={leftItem && leftItem.icon}
-            onIconClicked={leftItem && leftItem.onPress}
-            title={this.props.title}
-            titleColor={textColor}
-            subtitleColor={textColor}
-            actions={actions}
-            onActionSelected={this.handleActionSelected.bind(this)}
-            style={Styles.toolbar}>
-            {content}
-            </ToolbarAndroid>
+                <ToolbarAndroid
+                    navIcon={leftItem && leftItem.icon}
+                    onIconClicked={leftItem && leftItem.onPress}
+                    title={this.props.title}
+                    titleColor={textColor}
+                    subtitleColor={textColor}
+                    actions={actions}
+                    onActionSelected={() => { this.handleActionSelected(); }}
+                    style={Styles.toolbar}>
+                    {content}
+                </ToolbarAndroid>
             </View>
         );
     }
@@ -64,21 +77,30 @@ class F8HeaderAndroid extends React.Component {
             items = [this.props.rightItem, ...items];
         }
         const item = items[position];
-        item && item.onPress && item.onPress();
+        if (item && item.onPress) {
+            item.onPress();
+        }
     }
 }
 
 
 class F8HeaderIOS extends React.Component {
-    props: Props;
+    static propTypes = {
+        leftItem: React.PropTypes.object,
+        rightItem: React.PropTypes.object,
+        title: React.PropTypes.string,
+        style: React.PropTypes.number,
+        foreground: React.PropTypes.string,
+        children: React.PropTypes.element
+    };
 
     render() {
-        const {leftItem, title, rightItem, foreground} = this.props;
+        const { leftItem, title, rightItem, foreground } = this.props;
         const titleColor = foreground === 'dark' ? Colors.darkText : 'white';
         const itemsColor = foreground === 'dark' ? Colors.lightText : 'white';
 
         const content = React.Children.count(this.props.children) === 0
-        ? <Text style={[Styles.titleText, {color: titleColor}]}>
+        ? <Text style={[Styles.titleText, { color: titleColor }]}>
             {title}
         </Text>
         : this.props.children;
@@ -88,10 +110,9 @@ class F8HeaderIOS extends React.Component {
                     <ItemWrapperIOS color={itemsColor} item={leftItem} />
                 </View>
                 <View
-                accessible={true}
-                accessibilityLabel={title}
-                accessibilityTraits="header"
-                style={Styles.centerItem}>
+                    accessibilityLabel={title}
+                    accessibilityTraits="header"
+                    style={Styles.centerItem}>
                     {content}
                 </View>
                 <View style={Styles.rightItem}>
@@ -104,18 +125,23 @@ class F8HeaderIOS extends React.Component {
 }
 
 class ItemWrapperIOS extends React.Component {
+    static propTypes = {
+        item: React.PropTypes.object,
+        color: React.PropTypes.string
+    };
+
     render() {
-        const {item, color} = this.props;
+        const { item, color } = this.props;
         if (!item) {
             return null;
         }
 
         let content;
-        const {title, icon, layout, onPress} = item;
+        const { title, icon, layout, onPress } = item;
 
         if (layout !== 'icon' && title) {
             content = (
-                <Text style={[Styles.itemText, {color}]}>
+                <Text style={[Styles.itemText, { color }]}>
                     {title.toUpperCase()}
                 </Text>
             );
@@ -125,11 +151,11 @@ class ItemWrapperIOS extends React.Component {
 
         return (
             <TouchableOpacity
-            accessibilityLabel={title}
-            accessibilityTraits="button"
-            onPress={onPress}
-            style={Styles.itemWrapper}>
-            {content}
+                accessibilityLabel={title}
+                accessibilityTraits="button"
+                onPress={onPress}
+                style={Styles.itemWrapper}>
+                {content}
             </TouchableOpacity>
         );
     }
@@ -139,64 +165,3 @@ const Header = Platform.OS === 'ios' ? F8HeaderIOS : F8HeaderAndroid;
 Header.height = HEADER_HEIGHT;
 
 module.exports = Header;
-// module.exports.__cards__ = (define) => {
-//     const menuItem = {
-//         title: 'Menu',
-//         icon: require('./img/hamburger.png'),
-//         onPress: () => alert('Menu button pressed!'),
-//     };
-//     const filterItem = {
-//         title: 'Filter',
-//         icon: require('./img/filter.png'),
-//         onPress: () => alert('Filter button pressed!'),
-//     };
-//
-//     define('Simple', () => <Header title="Hello, world" />);
-//     define('With items', () => (
-//         <Header
-//         title="Default"
-//         leftItem={menuItem}
-//         rightItem={filterItem}
-//         />
-//     ));
-//     define('Forcing icons', () => (
-//         <Header
-//         title="Forcing icons"
-//         leftItem={{...menuItem, layout: 'icon'}}
-//         rightItem={{...filterItem, layout: 'icon'}}
-//         />
-//     ));
-//     define('Forcing title', () => (
-//         <Header
-//         title="Forcing title"
-//         leftItem={{...menuItem, layout: 'title'}}
-//         rightItem={{...filterItem, layout: 'title'}}
-//         />
-//     ));
-//     define('With content', () => (
-//         <Header leftItem={menuItem}>
-//         <View style={{backgroundColor: '#224488'}}>
-//         <Text style={{color: 'yellow'}}>
-//         Yellow text as title
-//         </Text>
-//         </View>
-//         </Header>
-//     ));
-//     define('With Background', () => (
-//         <Header
-//         title="With Background"
-//         leftItem={{...menuItem, layout: 'title'}}
-//         rightItem={{...filterItem, layout: 'title'}}
-//         style={{backgroundColor: '#224488'}}
-//         />
-//     ));
-//     define('With light background', () => (
-//         <Header
-//         title="Light Background"
-//         leftItem={{...menuItem, layout: 'title'}}
-//         rightItem={{...filterItem, layout: 'title'}}
-//         style={{backgroundColor: 'white'}}
-//         foreground="dark"
-//         />
-//     ));
-// };
